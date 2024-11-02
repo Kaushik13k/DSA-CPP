6e46645bd2ed76c22a4e511197ec327181d56a3b
Kaushik <kaushik13k@Apples-MacBook-Air.local>
2024-11-02 21:30:23 +0530

    addded the problem5

---
+++ Arrays/05maxAndMinSellingProce.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+        int bestBuy = prices[0];
+
+        for (int i = 1; i < prices.size(); i++){
+            if (prices[i] > bestBuy){
+                maxProfit = max(maxProfit, prices[i] - bestBuy);
+            }
+            bestBuy = min(bestBuy, prices[i]);
+        }
+        return maxProfit;
+    }
+};
